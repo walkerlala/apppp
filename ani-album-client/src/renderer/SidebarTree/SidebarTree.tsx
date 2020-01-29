4c9dc3ce553d78a8b7774b158347e85bfa146793
Vincent Chan <okcdz@diverse.space>
2020-01-29 20:02:49 +0800

    feat: add hover style to siidebar

--- ani-album-client/src/renderer/SidebarTree/SidebarTree.tsx
+++ ani-album-client/src/renderer/SidebarTree/SidebarTree.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import { produce } from 'immer';
+import SidebarTreeItem, { TreeItemData, TreeItemKey } from './SidebarTreeItem';
+import MediaServicesScaleLargeIcon from '@atlaskit/icon/glyph/media-services/scale-large';
+import FolderIcon from '@atlaskit/icon/glyph/folder';
+import DashboardIcon from '@atlaskit/icon/glyph/dashboard';
 import './SidebarTree.scss';
-import SidebarTreeItem, { TreeItemData } from './SidebarTreeItem';
 
 export interface SidebarTreeState {
   childrenMap: Map<string, TreeItemData[]>
@@ -9,29 +12,33 @@ export interface SidebarTreeState {
   selectedKey: string;
 }
 
-const PreserveRootKey = '__root';
-
 class SidebarTree extends React.Component<{}, SidebarTreeState> {
 
   constructor(props: {}) {
     super(props);
     const childrenMap: Map<string, TreeItemData[]> = new Map();
-    childrenMap.set(PreserveRootKey, [
+    childrenMap.set(TreeItemKey.Root, [
       {
+        key: TreeItemKey.MyPhotos,
         label: 'My Photos',
+        icon: <MediaServicesScaleLargeIcon label='My Photos' />
       },
       {
+        key: TreeItemKey.Albums,
         label: 'Albums',
+        icon: <FolderIcon label="Albums" />
       },
       {
+        key: TreeItemKey.Workspaces,
         label: 'Workspaces',
+        icon: <DashboardIcon label="Workspaces" />
       }
     ]);
 
     this.state = {
       childrenMap,
       expandedKeys: new Set(),
-      selectedKey: '',
+      selectedKey: TreeItemKey.MyPhotos,
     };
   }
 
@@ -49,14 +56,28 @@ class SidebarTree extends React.Component<{}, SidebarTreeState> {
     this.setState({ expandedKeys: selectedKeys });
   }
 
+  private handleTreeItemClick = (key: TreeItemKey) => (e: React.MouseEvent<HTMLDivElement>) => {
+    this.setState({
+      selectedKey: key,
+    });
+  }
+
   renderChildren(items: TreeItemData[]) {
+    const { selectedKey } = this.state;
     return items.map(item => {
-      return <SidebarTreeItem data={item} />
+      return (
+        <SidebarTreeItem
+          key={item.key}
+          isSelected={item.key === selectedKey}
+          data={item}
+          onClick={this.handleTreeItemClick(item.key)}
+        />
+      );
     })
   }
 
   render() {
-    const rootChildren = this.state.childrenMap.get(PreserveRootKey) || [];
+    const rootChildren = this.state.childrenMap.get(TreeItemKey.Root) || [];
     return (
       <div className="ani-sidebar-tree">
         {this.renderChildren(rootChildren)}
--- ani-album-client/src/renderer/SidebarTree/SidebarTreeItem.tsx
+++ ani-album-client/src/renderer/SidebarTree/SidebarTreeItem.tsx
@@ -1,19 +1,37 @@
 import * as React from 'react';
 
+export enum TreeItemKey {
+  Root = '__root',
+  MyPhotos = 'my-photos',
+  Albums = 'albums',
+  Workspaces = 'workspaces',
+}
+
 export interface TreeItemData {
+  key: TreeItemKey;
   label: string;
+  icon?: React.ReactNode;
 }
 
 export interface SidebarTreeItemProps {
   data: TreeItemData,
+  isSelected: boolean;
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
 class SidebarTreeItem extends React.PureComponent<SidebarTreeItemProps> {
 
   render() {
-    const { label } = this.props.data;
+    const { label, icon } = this.props.data;
+    let containerClassName = 'ani-sidebar-tree-item noselect';
+    if (this.props.isSelected) {
+      containerClassName += " item-selected";
+    }
     return (
-      <div className="ani-sidebar-tree-item noselect">
+      <div onClick={this.props.onClick} className={containerClassName}>
+        <div className="icon-area">
+          {icon}
+        </div>
         {label}
       </div>
     );
