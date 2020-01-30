1ce7cd9b4723fc0f270d39a6b322290a8681a7c8
Vincent Chan <okcdz@diverse.space>
2020-01-30 20:03:39 +0800

    feat: small ui adjustment

--- ani-album-client/src/renderer/Header/Header.tsx
+++ ani-album-client/src/renderer/Header/Header.tsx
@@ -9,6 +9,9 @@ class Header extends React.Component {
   render() {
     return (
       <div className="ani-header">
+        <div className="ani-big-heading noselect">
+          2019 年 1 月 30 日
+        </div>
         <div className="ani-header-button-group">
           <SearchBox />
           <Slider />
